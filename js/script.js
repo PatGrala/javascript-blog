858c27299c528f0fc5f3cfead5cb27ec6bf211cf
Patryk Grala <patrykgrala@gmail.com>
2023-02-01 02:57:38 +0100

    End of a task

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@
 function titleClickHandler(event)
 {	
 	const clickedElement = this;
+	event.preventDefault();
 	console.log('Link was clicked!');
 	console.log(event);
 
 	/* [Done] remove class 'active' from all article links  */
 
-	const activeLinks = document.querySelectorAll('.title a.active')
+	const activeLinks = document.querySelectorAll('.titles a.active')
 
 	for (let activeLink of activeLinks)
 	{
 		activeLink.classList.remove('active');
 	}
 
-	/* add class 'active' to the clicked link */
+	/* [Done] add class 'active' to the clicked link */
 
 	console.log('clickedElement:', clickedElement);
 	clickedElement.classList.add("active");
@@ -29,11 +30,20 @@ function titleClickHandler(event)
 		activeArticle.classList.remove('active');
 	}
 
-	/* get 'href' attribute from the clicked link */
+	/* [Done] get 'href' attribute from the clicked link */
 
-	/* find the correct article using the selector (value of 'href' attribute) */
+	const articleSelector = clickedElement.getAttribute('href');
+	console.log(articleSelector);
+
+	/* [Done] find the correct article using the selector (value of 'href' attribute) */
+
+	const targetArticle = document.querySelector(articleSelector);
+	console.log(targetArticle);
 
 	/* add class 'active' to the correct article */
+
+	targetArticle.classList.add("active");
+
 }
 
 const links = document.querySelectorAll('.titles a');
